45fbc59920541d0bffbf205a33705e5787e24775
Paloma Wink <paloma.wink@hotmail.com>
2023-06-05 16:17:10 -0300

    Configuracoes iniciais do provider, context, rotas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      {/*  <Route path="/meals" component={} /> //Tela principal de receitas de comidas
+      <Route path="/drinks" component={} /> //Tela principal de receitas de bebidas
+      <Route path="/meals/:id-da-receita" component={} /> //Tela de detalhes de uma receita de comida
+      <Route path="/drinks/:id-da-receita" component={} /> //Tela de detalhes de uma receita de bebida
+      <Route path="/meals/:id-da-receita/in-progress" component={} /> //Tela de receita em progresso de comida
+      <Route path="/drinks/:id-da-receita/in-progress" component={} /> //Tela de receita em progresso de bebida
+      <Route path="/perfil" component={} /> //Tela de perfil
+      <Route path="/done-recipes" component={} /> //Tela de receitas feitas
+      <Route path="/favorite-recipes" component={} /> //Tela de favoritos */}
+    </Switch>
   );
 }
 
---
+++ src/context/Provider.js
@@ -0,0 +1,23 @@
+import React, { useMemo, useState } from 'react';
+import Context from './Context';
+
+const INICIAL_STATE = {
+  email: '',
+  name: '',
+};
+
+function Provider({ children }) {
+  const [state, setState] = useState(INICIAL_STATE);
+
+  const value = useMemo(() => ({ state, setState }), [state, setState]);
+
+  return (
+    <Context.Provider value={ value }>
+      {children}
+    </Context.Provider>
+  );
+}
+Provider.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+export default Provider;
